225d774cd0158831e74f3711662bc57c654ff85b
JamesTLopez <jtlopez919@gmail.com>
2022-07-19 14:16:17 -0400

    init

---
+++ src/pages/FashionBlog/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Home from "./Sections/Home";
+
+function FashionBlog() {
+  return (
+    <Container>
+      <main>
+        <Home />
+      </main>
+    </Container>
+  );
+}
+
+export default FashionBlog;
+
+const Container = styled.div``;
