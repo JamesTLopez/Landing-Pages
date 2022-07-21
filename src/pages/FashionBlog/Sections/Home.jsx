a9b2fb9bbd44e85f37bd45b3678c26497cd439a0
JamesTLopez <jtlopez919@gmail.com>
2022-07-21 12:45:01 -0400

    added sub description area

--- src/pages/FashionBlog/Sections/Home.jsx
+++ src/pages/FashionBlog/Sections/Home.jsx
@@ -16,6 +16,24 @@ function Home() {
             <div className="img_container">
               <img src={Model1} alt="img-woman" />
             </div>
+            <div className="sub-description">
+              <h2>
+                Designer Women With
+                <br /> Beautiful Session
+              </h2>
+              <div className="bottom-description">
+                <p>
+                  Like So Many Things,The Race Was Canceled. Soon After, Gyms
+                  Around The Country Were Closed.Suddenlt, Running -- A Solo
+                  Activity That Can Be Done Outdoors, Away For Other People --
+                  Seems More Important Than Ever, For Both Our Health And Our
+                  Mood
+                </p>
+                <div className="img-arrow">
+                  <img src={ArrowDown} alt="Arrow" />
+                </div>
+              </div>
+            </div>
           </div>
         </BannerSection>
       </main>
@@ -36,7 +54,7 @@ const HomeSectionWrapper = styled.section`
     background: linear-gradient(
       to bottom,
       #eb5757 0%,
-      #eb5757 75%,
+      #eb5757 58%,
       #f5f2e9 50%,
       #f5f2e9 100%
     );
@@ -54,8 +72,9 @@ const BannerSection = styled.section`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    max-width: 1200px;
     padding: 0 25px 0 25px;
-    max-width: 1000px;
     .title {
       position: relative;
       display: flex;
@@ -80,8 +99,8 @@ const BannerSection = styled.section`
         position: relative;
         left: 50px;
         top: -30px;
-        height: 12rem;
-        width: 12rem;
+        height: 10rem;
+        width: 10rem;
       }
     }
     .img_container {
@@ -90,6 +109,39 @@ const BannerSection = styled.section`
         object-fit: cover;
         object-position: center;
         border: 5px solid black;
+      }
+    }
+
+    .sub-description {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      h2 {
+        margin: 20px 0 20px 0;
+        font-size: 7.5rem;
+        width: 100%;
+      }
+      .bottom-description {
+        display: flex;
+        align-items: center;
+
+        p {
+          flex: 1;
+          font-size: 1.7rem;
+          opacity: 0.8;
+        }
+        .img-arrow {
+          position: relative;
+          flex: 1;
+          img {
+            transform: rotate(-90deg);
+            height: 12rem;
+            width: 12rem;
+            padding: 10px;
+          }
+        }
       }
     }
   }
@@ -110,9 +162,35 @@ const BannerSection = styled.section`
           width: 10rem;
         }
       }
+      .sub-description {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        h2 {
+          margin: 30px 0 30px 0;
+          font-size: 5.5rem;
+        }
+        .bottom-description {
+          display: flex;
+          flex-direction: column;
+          p {
+            flex: 1;
+            font-size: 1.2rem;
+            opacity: 0.8;
+            text-align: center;
+          }
+          .img-arrow {
+            img {
+              transform: rotate(0deg);
+              height: 10rem;
+              width: 10rem;
+            }
+          }
+        }
+      }
     }
   }
-  @media screen and (max-width: 500px) {
+  @media screen and (max-width: 600px) {
     .container {
       .title {
         display: flex;
@@ -130,6 +208,12 @@ const BannerSection = styled.section`
           top: 0;
           height: 8rem;
           width: 8rem;
+        }
+      }
+      .sub-description {
+        h2 {
+          font-size: 4rem;
+          text-align: center;
         }
       }
     }
@@ -151,6 +235,7 @@ const BannerSection = styled.section`
   }
   @media screen and (max-width: 375px) {
     .container {
+      padding: 0 5px 0 5px;
       .title {
         width: 100%;
         padding: 0;
